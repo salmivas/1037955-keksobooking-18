17f1d5216bb0c0b69eeeb14b8ca6901e1b9bd03a
Ivan Salmin <salmin.iv@gmail.com>
2019-10-10 19:54:18 +0400

    Divides main.js on separate modules

---
+++ js/form.js
@@ -0,0 +1,29 @@
+'use strict';
+
+(function () {
+  var capacity = document.querySelector('#capacity');
+  var roomNumber = document.querySelector('#room_number');
+  var adFormSubmit = document.querySelector('.ad-form__submit');
+
+
+  var roomsCapacityCompare = function () {
+    var rooms = Number(roomNumber.value);
+    var guests = Number(capacity.value);
+
+    if (rooms === 1 && guests !== 1) {
+      capacity.setCustomValidity('1 комната для 1 гостя');
+    } else if (rooms === 2 && guests === 0 || guests > 2) {
+      capacity.setCustomValidity('2 комнаты не более чем для 2 гостей');
+    } else if (rooms === 3 && guests === 0 || guests > 3) {
+      capacity.setCustomValidity('3 комнаты не более чем для 3 гостей');
+    } else if (rooms === 100 && guests !== 0) {
+      capacity.setCustomValidity('100 комнат не для гостей');
+    } else {
+      capacity.setCustomValidity('');
+    }
+  };
+
+  roomNumber.addEventListener('change', roomsCapacityCompare);
+  capacity.addEventListener('change', roomsCapacityCompare);
+  adFormSubmit.addEventListener('click', roomsCapacityCompare);
+})();
